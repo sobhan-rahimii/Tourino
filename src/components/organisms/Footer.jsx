8f3e7c6a68cde48a3cdcd2b477e80525ca26fff0
sobhan rahimi <sobhanrahimidev@gmail.com>
2026-02-25 11:28:10 +0330

    .

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -7,83 +7,48 @@ import samandehi from "../../assets/images/icon/samandehi.png";
 import qrcode from "../../assets/images/icon/qrcode.png";
 import passenger from "../../assets/images/icon/passenger.png";
 import airline from "../../assets/images/icon/airline.png";
+import { icons } from "lucide-react";
+import Logo from "../atoms/icon/Logo";
 
 function Footer() {
   return (
     <>
-      <footer className="">
-        <hr className="w-[1188px] mr-[126px] ml-[126px] " />
-        <div className=" flex  mr-[126px]">
-          <div>
-            <ul>
-              <li className="mt-[21px]">تورینو</li>
-              <li className="mt-[24px]">درباره ما</li>
-              <li className="mt-[8px]">تماس با ما</li>
-              <li className="mt-[8px]">چرا تورینو</li>
-              <li className="mt-[8px]">بیمه مسافرتی</li>
+      <footer className="w-full">
+        <hr className="max-w-[1200px] mx-auto   " />
+          <div className=" max-w-[1200px] mr-[] mx-auto flex items-center justify-between  ">
+        <div className="flex">
+            <ul className="flex-col mr-[126px] mt-[21px] mb-[37px]  ">
+              <li>تورینو</li>
+              <li>درباره ما</li>
+              <li>تماس با ما</li>
+              <li>چرا تورینو</li>
+              <li>بیمه مسافرتی</li>
             </ul>
-          </div>
-          <div className="">
-            <ul>
-              <li className="mt-[21px] mr-[141px]">خدمات مشتریان</li>
-              <li className="mt-[24px] mr-[143px]">پشتیبانی آنلاین</li>
-              <li className="mt-[8px] mr-[126px]">راهنمای خرید</li>
-              <li className="mr-[131px] mt-[8px]">راهنمای استرداد</li>
-              <li className="mt-[8px] mr-[100px]">پرسش و پاسخ</li>
+            <ul className="flex-col mt-[21px] mr-[141px]">
+              <li>خدمات مشتریان</li>
+              <li>پشتیبانی انلاین</li>
+              <li>راهنمای خرید</li>
+              <li>راهنمای استرداد</li>
+              <li>پرسش و پاسخ</li>
             </ul>
-          </div>
-          <div className="  flex-wrap items-start  ">
-            <Image
-              src={LogoImg}
-              width={146}
-              height={44}
-              alt="logo"
-              className="mr-[661px] mt-[30px]"
-            />
-            <div className="">
-              <p className="mt-[20px] mr-[686px]  ">تلفن پشتیبانی:021-8574</p>
-              <div className="flex">
-                <Image
-                  src={airline}
-                  width={78}
-                  height={74}
-                  alt="airline"
-                  className="mr-[393px] mt-[153px] mb-[28px]"
-                />
-                <Image
-                  src={passenger}
-                  width={71}
-                  height={74}
-                  alt="pass"
-                  className="mr-[32px] mt-[153px] mb-[28px]"
-                />
-                <Image
-                  src={qrcode}
-                  width={68}
-                  height={74}
-                  alt="pass"
-                  className="mr-[32px] mt-[153px] mb-[28px]"
-                />
-                <Image
-                  src={samandehi}
-                  width={67}
-                  height={74}
-                  alt="pass"
-                  className="mr-[32px] mt-[153px] mb-[28px]"
-                />
-                <Image
-                  src={aria}
-                  width={68}
-                  height={74}
-                  alt="pass"
-                  className="mr-[32px] mt-[153px] mb-[28px]"
-                />
+        </div>
+            <div className="flex-col mt-[30px]  ">
+              <Image src={LogoImg} width={146} height={44} alt="logo" />
+              <p>تلفن پشتیبانی:021-8574</p>
+              <div className="flex mt-[36px] mb-[28px] items-end gap-5  ">
+                <Image src={aria} width={68} height={74} alt="aria"/>
+                <Image src={samandehi} width={67} height={74} alt="saman"/>
+                <Image src={qrcode} width={67} height={74} alt="QR"/>
+                <Image src={passenger} width={67} height={74} alt="p"/>
+                <Image src={airline} width={67} height={74} alt="m"/>
+
               </div>
             </div>
           </div>
-        </div>
         <hr className="  w-full border border-solid border-[#00000033]" />
-        <p className="text-center mr-auto ml-auto mt-[12px] mb-[12px] ">کلیه حقوق این وب سایت متعلق به تورینو میباشد.</p>
+        <p className="text-center mr-auto ml-auto mt-[12px] mb-[12px] ">
+          کلیه حقوق این وب سایت متعلق به تورینو میباشد.
+        </p>
       </footer>
     </>
   );
--- src/components/organisms/WhyTourino.jsx
+++ src/components/organisms/WhyTourino.jsx
@@ -5,7 +5,28 @@ import questionn from "../../assets/images/icon/questionn.png";
 import Slider from "./Slider";
 import group1 from "../../assets/images/icon/group1.png";
 import messageicon from "../../assets/images/icon/messageicon.png";
-import rezayaticon from "../../assets/images/icon/rezayaticon.png"
+import rezayaticon from "../../assets/images/icon/rezayaticon.png";
+
+const features = [
+  {
+    id: 1,
+    title: "به صرفه ترین قیمت",
+    desc: "بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.",
+    icon: group1,
+  },
+  {
+    id: 2,
+    title: "پشتیبانی",
+    desc: "پشتیبانی و همراهی ۲۴ ساعته در تمامی مراحل سفر شما.",
+    icon: messageicon,
+  },
+  {
+    id: 3,
+    title: "رضایت کاربران",
+    desc: "رضایت بیش از ۱۰ هزار کاربر از تور های ما.",
+    icon: rezayaticon,
+  },
+];
 
 function WhyTourino() {
   return (
@@ -25,7 +46,9 @@ function WhyTourino() {
 
           <span className="mr-[15px] content-center ">چراتورینو؟</span>
         </div>
-        <h1 className="mr-[152px] mt-[32px] hidden lg:block ">تور طبیعت گردی</h1>
+        <h1 className="mr-[152px] mt-[32px] hidden lg:block ">
+          تور طبیعت گردی
+        </h1>
         <div className="lg:flex flex-row flex-col   ">
           <p className="w-[517px] h-[264px] font-[400] hidden lg:block text-[20px] leading-[220.00000000000003%] mr-[152px] mt-[16px] ">
             اگر دوست داشته باشید که یک جاذبه طبیعی را از نزدیک ببینید و در دل
@@ -34,52 +57,58 @@ function WhyTourino() {
             آثار تاریخی یک مقصد خاص بازدید کنید، می‌توانید تورهای فرهنگی و
             تاریخی را خریداری کنید.
           </p>
-           <div className=" inline-block mr-[31px] mt-[16px]">
-          <Slider />
-
-            </div> 
+          <div className=" inline-block mr-[31px] mt-[16px]">
+            <Slider />
+          </div>
           {/* border: 1px solid #00000033 */}
         </div>
         <hr className="w-full border border-solid border-[#00000033 ]  " />
       </div>
-      <div className="flex">
-        <Image
-          src={group1}
-          width={121.42}
-          height={109.03}
-          alt="gr"
-          className="mr-[152.47px] mt-[23px]"
-        />
-        <div className="  flex-wrap">
-          <h1 className="mt-[30px] mr-[8.11px]">به صرفه ترین قیمت</h1>
+      <div></div>
+      <div className="flex  flex-col text-wrap mr-[150px]  lg:max-w-[1200px] w-full  lg:flex-row justify-center lg:mt-[33px]  ml-[29px] gap-[40px] ">
+        <div className="flex  items-center w-full lg:w-auto">
+          <Image
+            src={group1}
+            width={121.42}
+            height={109.03}
+            alt="gr"
+            className=""
+          />
+          <div className="  flex flex-col">
+            <h1 className="">به صرفه ترین قیمت</h1>
 
-          <p className=" w-[202px] mt-[4px] mr-[8.11px] font-[300] text-[16px] leading-[100%]  ">
-            بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.
-          </p>
+            <p className=" max-w-[180px]  ">
+              بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.
+            </p>
+          </div>
         </div>
 
-        <div className="flex">
+        <div className="flex items-center w-full lg:w-auto">
           <Image
             src={messageicon}
             width={109.03}
             height={99.12}
             alt="message"
-            className="mr-[66.89px] mt-[31px] "
+            className=" "
           />
-          <div className="flex-wrap">
-            <h1 className="mt-[33px] mr-[8.08px]">پشتیبانی</h1>
-            <p className="mt-[4px] mr-[8.08px] w-[248px]  ">
+          <div className="flex flex-col ">
+            <h1 className="">پشتیبانی</h1>
+            <p className=" max-w-[180px] ">
               پشتیبانی و همراهی 24 ساعته در تمامی مراحل سفر شما.
             </p>
           </div>
-          <div className="flex">
-            <Image src={rezayaticon} width={104.08} height={104.08} alt="rezayat" className="mt-[32.91px] mr-[66.92px]" />
-            <div className="flex-wrap">
-                <h1 className="mt-[30px] mr-[8px] ">رضایت کاربران</h1>
-                <p className="w-[192px] mt-[4px] mr-[8px]">رضایت بیش از 10هزار کاربر از تور های ما. </p>
-            </div>
+        </div>
+        <div className="flex items-center">
+          <Image
+            src={rezayaticon}
+            width={104.08}
+            height={104.08}
+            alt="rezayat"
+          />
+          <div className="flex flex-col">
+            <h1 className="">رضایت کاربران</h1>
+            <p className="max-w-[180px]">رضایت بیش از 10هزار کاربر از تور های ما. </p>
           </div>
-
         </div>
       </div>
     </>
